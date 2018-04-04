65db0816cb80aac8104dbcf40a289158409ca48f
coleb <coleb@DESKTOP-LDSFCLM>
2018-04-04 10:15:31 -0400

    added more

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="canvas.py",
+    description="Python implementation of the Canvas API.",
+    version='1.0.0.dev1',
+    license='MIT',
+    author="Cole Brooks",
+    author_email="cole@colebrooks.net"
+)
